8471c73febf4c9653bb4b33a172744058ff158e4
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-24 20:21:44 +0500

    Task6

---
+++ Tyuiu.ShakirovaGM.Sprint2.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShakirovaGM.Sprint2.Task6.V4.Lib
+{
+    public class DataService : ISprint2Task6V4
+    {
+        public string FindCardSuit(int value)
+        {
+            string res;
+            switch (value)
+            {
+                case 1: return "пики";
+                case 2: return "трефы";
+                case 3: return "бубны";
+                case 4: return "червы";
+                default:throw new ArgumentException($"Номер масти должен быть от 1 до 4. Значение {value} ");
+
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint2.Task6.V4/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ShakirovaGM.Sprint2.Task6.V4.Lib;
+namespace Tyuiu.ShakirovaGM.Sprint2.Task6.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Шакирова Г. М. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Шакирова Гузель Мансуровна | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Мастям игральных карт условно присвоены следующие порядковые номера:    *");
+            Console.WriteLine("* масти «пики»—1, масти «трефы»—2, масти «бубны»—3, масти «червы»—4.      *");
+            Console.WriteLine("* По заданному номеру масти m (1 <= m <= 4) определить название           *");
+            Console.WriteLine("* соответствующей масти.                                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            Console.WriteLine("Введите номер масти: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((m < 1) || (m > 4))
+                res = "Введено неверное значение";
+            else
+                res = "Это масть " + ds.FindCardSuit(m);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+    
+}
